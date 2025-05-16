d81c02a371bf77577acdb9093c443b10b72f63ff
Kosalrak <linnorak.k@aditi.asia>
2025-05-16 09:12:55 +0700

    frontend

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // Listen on all interfaces
+    port: 3000 // Match Dokploy’s expected port
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000
+  }
 })
